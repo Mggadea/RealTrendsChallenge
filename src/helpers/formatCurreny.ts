eece7c6940dc2a1bb8d37b8d154ad5f151129f99
mggadea <matiasgaraygadea@outlook.com>
2024-06-30 14:41:01 -0300

    add helper

---
+++ src/helpers/formatCurreny.ts
@@ -0,0 +1,9 @@
+export const formatCurrency = (value) => {
+    return new Intl.NumberFormat('es-AR', {
+      style: 'currency',
+      currency: 'ARS',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).format(value);
+  };
+  
