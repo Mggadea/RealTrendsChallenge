b96bb0566dc6a249e72d2b4fa0533005246b5924
mggadea <matiasgaraygadea@outlook.com>
2024-07-02 14:52:24 -0300

    add screens

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
-const details = () => {
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {formatCurrency} from '../helpers/formatCurreny';
+
+const DetailsScreen = () => {
   return (
-    <View>
-      <Text>details</Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>
+        Moto G6 Plus 4gb/64gb Reacondicionado
+      </Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: 'https://http2.mlstatic.com/D_700299-MLA76251159053_052024-O.jpg',
+        }}
+        onError={error =>
+          console.log('Image Load Error:', error.nativeEvent.error)
+        }
+      />
+      <Image />
+      <Text style={styles.priceText}>{formatCurrency(140000)}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default details
+export default DetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  titleText: {
+    fontSize: 18,
+    
+    marginVertical: 20,
+    },
+  priceText: {
+    fontSize: 30,
+
+  },
+});
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,17 +1,21 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React, {useEffect,useState} from 'react'
+/* eslint-disable no-catch-shadow */
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import SearchBar from '../components/searchBar';
 import Product from '../components/product';
 
 
-const Home = () => {
+const HomeScreen = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const [searchText, setSearchText] = useState<string>('');
+
   useEffect(() => {
-    const apiUrl = 'https://api.mercadolibre.com/sites/MLA/search?q=Motorola%20G6';
+    const apiUrl =
+      'https://api.mercadolibre.com/sites/MLA/search?q=Motorola%20G6';
     const fetchData = async () => {
       try {
         const response = await axios.get(apiUrl);
@@ -26,24 +30,29 @@ const Home = () => {
     fetchData();
   }, []);
 
-  
+  const filteredData = data.filter(item =>
+    item?.title.toLowerCase().includes(searchText.toLowerCase()),
+  );
+
   return (
     <View style={styles.container}>
-      <SearchBar />
+      <SearchBar searchText={searchText} setSearchText={setSearchText} />
+
       <FlatList
-        data={data}
+      style={{flex:1,}}
+        data={filteredData}
         renderItem={({item}) => <Product item={item} />}
         keyExtractor={item => item.id}
       />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
+    flex:1,
     backgroundColor: '#fff',
-
-  }
-})
+  },
+});
